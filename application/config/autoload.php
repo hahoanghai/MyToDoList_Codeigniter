398be7befa170a57e351d20145232d250e390311
hahoanghai <hahoanghai.ueh@gmail.com>
2016-02-28 18:00:21 -0800

    3 model for user, list, task

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -126,4 +126,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('Task_model','List_model','User_model');
